47e0fb95bb4b4a3d0383b31a6132c49a3a4fc65d
mkobar <mike@exlorev.com>
2019-01-03 16:02:01 -0500

    simple index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+// index.js
+var express = require('express');
+var app = express();
+
+/***
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
+***/
+
+// This responds with "Hello World" on the homepage
+app.get('/', function (req, res) {
+   console.log("Got a GET request for the homepage");
+   res.send('Hello GET');
+})
+
+// This responds a POST request for the homepage
+app.post('/', function (req, res) {
+   console.log("Got a POST request for the homepage");
+   res.send('Hello POST');
+})
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
+
