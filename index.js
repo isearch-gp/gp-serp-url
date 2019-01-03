12c24890aea6d16660be51ba3228fcbfae1723a9
mkobar <mike@exlorev.com>
2019-01-03 16:06:23 -0500

    form with json

--- index.js
+++ index.js
@@ -2,16 +2,35 @@
 var express = require('express');
 var app = express();
 
+// static pages
+app.use(express.static('public'));
+
 /***
 app.get('/', function (req, res) {
    res.send('Hello World');
 })
 ***/
 
+/***
 // This responds with "Hello World" on the homepage
 app.get('/', function (req, res) {
    console.log("Got a GET request for the homepage");
    res.send('Hello GET');
+})
+***/
+
+app.get('/', function (req, res) {
+   res.sendFile( __dirname + "/" + "search.html" );
+})
+
+app.get('/process_get', function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      first_name:req.query.first_name,
+      last_name:req.query.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
 })
 
 // This responds a POST request for the homepage
