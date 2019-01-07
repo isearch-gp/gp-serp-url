67f7efb85c44a6f1c6d71e385d093c16fb807c31
mkobar <mike@exlorev.com>
2019-01-07 16:48:38 -0500

    back to 8081

--- index.js
+++ index.js
@@ -235,7 +235,7 @@ app.post('/process_googler', urlencodedParser, function (req, res) {
 })
 
 //var server = app.listen(8081, function () {
-var server = app.listen(80, function () {
+var server = app.listen(8081, function () {
    var host = server.address().address
    var port = server.address().port
    
